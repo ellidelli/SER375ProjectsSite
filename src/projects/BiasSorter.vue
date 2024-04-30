9875c7eff51e88fa3d89597d68b2c2150353ce43
shanbrick <sbrickett27@gmail.com>
2024-04-30 11:54:27 -0400

    finished everything. just need to add video

--- src/projects/BiasSorter.vue
+++ src/projects/BiasSorter.vue
@@ -18,22 +18,34 @@
                 </p>
             </template>
             <template v-slot:story>
-                <p>I wanted to make this website because as a K-Pop fan, I like to categorize my favorites into
+                <p>
+                    I wanted to make this website because as a K-Pop fan, I like to categorize my favorites into
                     categories. Prior to this website, I would use a spreadsheet. One sheet would display all of my
                     favorites in alphabetical order of the group, and another sheet displayed my favorites in the
                     categories I created. I ran into an issue where I would add someone to one sheet and forget to
                     add them to the other sheet. I thought to myself, "I wish this could just be automated!" This
                     was at the same time we had to think of an idea for this project. So that's what I decided to do!
+                    <br><br>
+                    In order to make this project, I used the Vue framework, which makes it easier to use HTML, CSS, and
+                    JavaScript. My earlier workplans were creating the bare bones of the website with just HTML, CSS,
+                    and basic JavaScript. Then halfway through, I transitioned over to the Vue framework. That's when I
+                    implemented more of the harder features, like the draggable elements. Since I had a big interest in
+                    the website, and I picked up on the code well, I was able to get more done than I had intented, so I
+                    was never struggling to complete anything on time.
                 </p>
             </template>
             <template v-slot:strugglesAndTriumphs>
-                <p>My biggest struggle with this project was making it so users could have their own log-ins to the
+                <p>
+                    My biggest struggle with this project was making it so users could have their own log-ins to the
                     website. I did this using Firebase Authorization and Firestore. As my project uses the Vue
                     framework, it was hard to find a tutorial that used data as complex as mine. It took many failed
                     attempts and guidance from Professor Thimineur to finally get it to work. I was close to giving up
                     and let the user's data be based on the browser's local storage, but luckily I was able to figure it
                     out. Now, a user can log-in to their account on separate browsers and devices and keep their data.
-                    <b>Tip: <i>Don't</i> use the Vuefire framework!!!</b>
+                    <b>Tip: <i>Don't</i> use the Vuefire API!!!</b>
+                    <br><br>
+                    Once I was able to get the users to work, that was a big win! My website was finally completed other
+                    than little details and extra stuff that I wanted to add.
                 </p>
             </template>
             <template v-slot:links>
@@ -52,8 +64,10 @@
 
 <script>
 import twoColumnTemplate from '../components/templates/twoColumnTemplate.vue'
-import homePage from '../projects/project_images/BrickettHomePage.png';
-import listPage from '../projects/project_images/BrickettListPage.png';
+import home from '../projects/project_images/BrickettHome.png';
+import list from '../projects/project_images/BrickettList.png';
+import groups from '../projects/project_images/BrickettGroups.png';
+import birthdays from '../projects/project_images/BrickettBirthday.png';
 // import sampleVideo from '../projects/project_videos/<VIDEO TITLE>'
 
 export default {
@@ -65,14 +79,14 @@ export default {
         return {
             projectTitle: "Bias Sorter",
             author: "Shannon Brickett",
-            pictures: [homePage, listPage],
+            pictures: [home, list, groups, birthdays],
             video: "sampleVideo" //get rid of the quotes when putting in your video variable
         }
     },
     info: {
         title: 'Bias Sorter',
         author: 'Shannon Brickett',
-        tags: ["Vue", "Website", "JavaScript", "HTML", "CSS"]
+        tags: ["Vue", "Website", "JavaScript", "HTML", "CSS", "Spring 2024"]
     }
 }
 </script>
