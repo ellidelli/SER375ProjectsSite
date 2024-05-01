fd333f1dd421eb31e4c5ea08b0eee4eaca39253c
jaysalgado <jaysalgado97@gmail.com>
2024-05-01 12:52:45 -0400

    complete

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/SER375ProjectsSite/" : "/",
-  transpileDependencies: true
-})
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/SER375ProjectsSite/" : "/",
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    // Rule for video files
+    config.module
+      .rule("videos")
+      .test(/\.(mov|mp4)$/)
+      .use("file-loader")
+      .loader("file-loader")
+      .options({
+        name: "[name].[hash:8].[ext]",
+      })
+      .end();
+  },
+});
