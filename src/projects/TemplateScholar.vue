6c6f6e57ce5fdd74a5d2d0754a3f66078356b43e
pferhan <hannah.pfersch@quinnipiac.edu>
2024-05-02 13:58:06 -0400

    template scholar page

--- src/projects/TemplateScholar.vue
+++ src/projects/TemplateScholar.vue
@@ -4,20 +4,24 @@
         <template v-slot:description>
           <p>This program offers a one-stop shop for all academic needs. Whether it is automated formatting of 
             academic posters or helping you build a professional looking resume, Template Scholar has the resources 
-            for you.</p>
+            for you. This website targets students, specifically those in higher education and new graduates.</p>
         </template>
         <template v-slot:story>
-          <p>This project originated from the constant need for students to be making professional looking presentations, 
-            posters, and resumes. As a soon to be new graduate, I found myself constantly updating and fighting my resume. 
-            This project works elimate the time spent formatting to allow users more time to be productive in other ways.
+          <p>This project originated from the constant need for students to make professional looking presentations, 
+            posters, and resumes. I felt like I was performing similar tasks over and over. In addition, as a soon to be 
+            new graduate I found myself constantly updating and fighting my resume. This project works eliminate the time 
+            spent formatting to allow users more time to be productive in other ways. To develop this project I learned HTML, 
+            CSS, JavaScript, and the tranfered everthing into Vue. This was a huge learning curve for me but a great experience
+            realizing I can learn as I go and I don't have to know everything right away. I took my workplans one at a time and 
+            had realistic expectations for my development.
           </p>
         </template>
         <template v-slot:strugglesAndTriumphs>
           <p>This project turned out to be even larger than I thought. I originally started with one idea and it just kept 
-            expanding to a website that has many different resources. My struggles in learning HTML, CSS, JavaScript and then 
-            transfering and learning Vue tookmup much of my time. In addition, formatting a pdf export proved to take up much 
-            more time than anticipated.
-          </p>
+            expanding to a website that has many different resources. Before transfering to using the Vue framework my big idea was 
+            difficult to maintain in separate HTML, CSS, and JS files. In addition, formatting a pdf export proved to take up much 
+            more time than anticipated. I was only able to complete the resume template but hope to expand on this project in the future.
+          </p>”
         </template>
         <template v-slot:links>
           <div style="padding-bottom: 10vh;">
@@ -32,9 +36,8 @@
   import twoColumnTemplate from '../components/templates/twoColumnTemplate.vue'
   import homepage_image from '../projects/project_images/tshome.png';
   import resume1_image from '../projects/project_images/tsresume1.png';
-  import resume2_image from '../projects/project_images/tsresume2.png';
-  import resume3_image from '../projects/project_images/tsresume3.png';
-  // import sampleVideo from '../projects/project_videos/<VIDEO TITLE>'
+  import complete_resume from '../projects/project_images/tsgeneratedresume.png';
+  import sampleVideo from '../projects/project_videos/tsvideodemo.mp4'
   
   export default {
     name: 'TemplateScholar',
@@ -45,14 +48,14 @@
       return {
         projectTitle: "Template Scholar",
         author: "Hannah Pfersch",
-        pictures: [homepage_image, resume1_image, resume2_image, resume3_image],
-        video: "sampleVideo" //get rid of the quotes when putting in your video variable
+        pictures: [homepage_image, resume1_image, complete_resume],
+        video: sampleVideo
       }
     },
     info: {
       title: 'Template Scholar',
       author: 'Hannah Pfersch',
-      tags: ["Vue", "Templates", "Open Source", "HTML", "CSS", "JavaScript", "Academic"]
+      tags: ["Vue", "Templates", "Open Source", "HTML", "CSS", "JavaScript", "Academic", "Spring 2024"]
     }
   }
   </script>
