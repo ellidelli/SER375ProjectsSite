16795d7b0bb0604951b7cfe8f451fbf387edabe8
ellidelli <elizabeth.delea@gmail.com>
2024-05-09 11:57:09 -0400

    fixes

--- src/projects/MafiaApp.vue
+++ src/projects/MafiaApp.vue
@@ -1,42 +1,52 @@
 <template>
   <div>
-    <oneColumnTemplate
-      :projectTitle="projectTitle"
-      :author="author"
-      :pictures="pictures"
-      :video="video"
-    >
+    <oneColumnTemplate :projectTitle="projectTitle" :author="author" :pictures="pictures" :video="video">
       <template v-slot:description>
         <p>
-          This is an iOS app for playing the party game “Mafia”. 
-          Each player joins from their phone and the software moderates the game. 
-          At the start each player is assigned a random role and the game proceeds to go through night and day cycles. 
-          During the night, players use their respective powers. (The mafia can kill someone, the doctor can save someone etc.) 
-          During the day, players vote for who should be eliminated. 
+          This is an iOS app for playing the party game “Mafia”.
+          Each player joins from their phone and the software moderates the game.
+          At the start each player is assigned a random role and the game proceeds to go through night and day cycles.
+          During the night, players use their respective powers. (The mafia can kill someone, the doctor can save someone
+          etc.)
+          During the day, players vote for who should be eliminated.
           The game continues until there is a winner according to the rule of Mafia.
         </p>
       </template>
       <template v-slot:story>
         <p>
-          This project was inspired by my love of social games. My fondest memories involve playing games with friends, especially ones that drew everyone in and got them involved. Mafia was always the epitome of this. 
-          I love how it tests acting skills and analytical skills and how it sometimes erupts into heated back and fourths. However, it does come with a caveat, one person has to sit out to moderate the game. 
-          It struck me that the moderator’s tasks are completely robotic, so why shouldn’t a robot do them? This exactly what I came to fix, with this app everyone can join in the game and the software does the technical work. 
-          Additionally, it avoids the long periods where players have to keep their eyes closed as they wait for others to go. Mafia seems like it was meant to have an app. So here it is, the code runs the game while the players get to focus on playing.
+          This project was inspired by my love of social games. My fondest memories involve playing games with friends,
+          especially ones that drew everyone in and got them involved. Mafia was always the epitome of this.
+          I love how it tests acting skills and analytical skills and how it sometimes erupts into heated back and
+          fourths. However, it does come with a caveat, one person has to sit out to moderate the game.
+          It struck me that the moderator’s tasks are completely robotic, so why shouldn’t a robot do them? This exactly
+          what I came to fix, with this app everyone can join in the game and the software does the technical work.
+          Additionally, it avoids the long periods where players have to keep their eyes closed as they wait for others to
+          go. Mafia seems like it was meant to have an app. So here it is, the code runs the game while the players get to
+          focus on playing.
 
-          In order to do this project I had to learn Swift, Firebase, and GitHub. But more generally, I learned how to develop the front-end and back-end of an app. These skills are valuable and will continue to come in handy. 
+          In order to do this project I had to learn Swift, Firebase, and GitHub. But more generally, I learned how to
+          develop the front-end and back-end of an app. These skills are valuable and will continue to come in handy.
 
-          In the beginning I was unsure of how to structure the project. I wasn’t yet familiar with app development so it was difficult to predict how long each thing would take. In the end however, a wonderful structure fell into place. 
-          The first few work-cycles were spent implementing the fundamentals of an app, both on the front-end and back-end. The remaining cycles were spent implementing the game. 
-          This was done in three parts; One, writing out the game logic on paper; Two, creating game-play functions that communicate with the back-end; Three, creating the game itself. 
+          In the beginning I was unsure of how to structure the project. I wasn’t yet familiar with app development so it
+          was difficult to predict how long each thing would take. In the end however, a wonderful structure fell into
+          place.
+          The first few work-cycles were spent implementing the fundamentals of an app, both on the front-end and
+          back-end. The remaining cycles were spent implementing the game.
+          This was done in three parts; One, writing out the game logic on paper; Two, creating game-play functions that
+          communicate with the back-end; Three, creating the game itself.
           This turned out to be a great structure and I plan to return to it in future app development.
         </p>
       </template>
       <template v-slot:strugglesAndTriumphs>
         <p>
-          The biggest struggle for me was learning new technology from scratch. This project required me to step way out of my comfort zone, and to jump in to tasks without knowing how to finish them. 
-          At the beginning I would be held back by minor syntax and technical errors and it was only through countless google searches and YouTube tutorials that I managed to move forward. 
-          However, at the end of the project I finally had enough skills and I was able to move much quicker towards the finish line. There were accomplishments along the way that encouraged the process. 
-          One notable one was when I connected the app to firebase for the first time and sent information from one phone to another. I was also fortunate to have a great professor who provided both technical help and encouragement.
+          The biggest struggle for me was learning new technology from scratch. This project required me to step way out
+          of my comfort zone, and to jump in to tasks without knowing how to finish them.
+          At the beginning I would be held back by minor syntax and technical errors and it was only through countless
+          google searches and YouTube tutorials that I managed to move forward.
+          However, at the end of the project I finally had enough skills and I was able to move much quicker towards the
+          finish line. There were accomplishments along the way that encouraged the process.
+          One notable one was when I connected the app to firebase for the first time and sent information from one phone
+          to another. I was also fortunate to have a great professor who provided both technical help and encouragement.
         </p>
       </template>
       <template v-slot:links>
@@ -64,14 +74,14 @@ export default {
   },
   data() {
     return {
-      projectTitle: "MafiaApp",
+      projectTitle: "Mafia App",
       author: "Sam Feld",
       pictures: [sampleImage1, sampleImage2, sampleImage3, sampleImage4],
       video: sampleVideo, //get rid of the quotes when putting in your video variable
     };
   },
   info: {
-    title: "Mafia iOS app",
+    title: "Mafia App",
     author: "Sam Feld",
     tags: ["Spring 2024", "iPhone", "Open Source", "Swift", "SwiftUI", "Xcode", "Firebase", "Game", "Mafia", "Party", "Fun"],
   },
